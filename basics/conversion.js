a7dee8364cbfbec09eb78f36a282e4d10ab4ce68
viditsharma1405 <137794570+viditsharma1405@users.noreply.github.com>
2024-07-07 17:51:50 +0000

    COMIT DONE

---
+++ basics/conversion.js
@@ -0,0 +1,8 @@
+let score="vidit"
+
+console.log(typeof score);
+
+let inNum=Number(score);//convert one data type to other
+console.log(inNum)
+let inNumm=String(score);//convert one data type to other
+console.log(inNumm)
---
+++ basics/variables.js
@@ -0,0 +1,6 @@
+const account_id=847573
+let password="362526"
+var city="jaipur"
+state="rajasthan"
+
+console.table([account_id,password,city,state])
