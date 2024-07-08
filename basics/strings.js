2282bad864a8843da6bb8adc46f1b339808bd188
viditsharma1405 <137794570+viditsharma1405@users.noreply.github.com>
2024-07-08 19:27:47 +0000

    strings

--- basics/strings.js
+++ basics/strings.js
@@ -2,34 +2,30 @@ const name = "hitesh"
 const repoCount = 50
 
 // console.log(name + repoCount + " Value");
+// long string
+// new string
+// length
+//uppercase
+//char by index
+// index of 
+// substring
+// slicing
+//trim
+//replace
+//search
+// array
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
-
-const gameName = new String('hitesh-hc-com')
-
-// console.log(gameName[0]);
-// console.log(gameName.__proto__);
-
-
-// console.log(gameName.length);
-// console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('t'));
-
-const newString = gameName.substring(0, 4)
-console.log(newString);
-
-const anotherString = gameName.slice(-8, 4)
-console.log(anotherString);
-
-const newStringOne = "   hitesh    "
-console.log(newStringOne);
-console.log(newStringOne.trim());
-
-const url = "https://hitesh.com/hitesh%20choudhary"
-
-console.log(url.replace('%20', '-'))
-
-console.log(url.includes('sundar'))
-
-console.log(gameName.split('-'));
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+const gamename = new String('vidit sharma-84878.d')
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.toLowerCase());
+console.log(gamename.charAt(5))
+console.log(gamename.indexOf('s'));
+console.log(gamename.substring(2,16));
+console.log(gamename.slice(-2,13));
+const newgame=new String('    jwedheh  ')
+console.log(newgame.trim())
+console.log(gamename.replace(' ','-'))
+console.log(gamename.includes('sharma'));
+console.log(gamename.split(' '));
